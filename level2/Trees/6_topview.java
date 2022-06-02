22eeb47d6777b68e50bd03d0dbfb30deb14fc57a
Aakash-Kumar-karn <akashkarn148@gmail.com>
2022-06-02 20:07:14 +0530

    first

---
+++ level2/Trees/6_topview.java
@@ -0,0 +1,54 @@
+class Solution
+{
+    static class Pair{
+        Node node ;
+        int hozi= 0;
+    }
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        ArrayList<Integer> retVal = new ArrayList<>();
+        HashMap<Integer,Node> map = new HashMap<>();
+         int lv = 0 , rv = 0 ;
+        
+        
+        Queue<Pair> queue = new LinkedList<>();
+        Pair p= new Pair();
+        p.node = root;
+        p.hozi = 0;
+        
+        queue.add(p);
+       
+        while(queue.size()>0){
+            Pair temp = queue.remove();
+             
+             if(temp.hozi<lv){
+                 lv=temp.hozi;
+             }
+              if(temp.hozi>rv){
+                 rv=temp.hozi;
+             }
+             if(map.containsKey(temp.hozi)==false){
+                 map.put(temp.hozi,temp.node);
+             }
+             if(temp.node.left != null){
+                 Pair lp =  new Pair();
+                 lp.node = temp.node.left;
+                 lp.hozi = temp.hozi-1;
+                 
+                 queue.add(lp);
+             }
+              if(temp.node.right != null){
+                 Pair rp =  new Pair();
+                 rp.node = temp.node.right;
+                 rp.hozi = temp.hozi+1;
+                 queue.add(rp);
+             }
+        }
+        for(int i = lv ; i<=rv  ; i++ ){
+            retVal.add(map.get(i).data);
+        }
+        return retVal;
+    }
+}
