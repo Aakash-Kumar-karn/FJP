5ebe753c1b921843bcd1d65e429b5be80ae188bf
Aakash-Kumar-karn <akashkarn148@gmail.com>
2022-06-02 13:10:49 +0530

    first

--- level2/Trees/1_LO.java
+++ level2/Trees/1_LO.java
@@ -20,3 +20,36 @@ public static void levelOrder(Node node) {
             System.out.println();
         }
      }
+
+     // with null appraoch lO linewise
+
+      public static void levelOrder(Node node) {
+    Queue<Node> ans = new LinkedList<>();
+    ans.add(node);
+    ans.add(null);
+    
+    while(ans.size() != 0){
+        int size =  ans.size();
+        while(size-->0){
+            Node rem = ans.remove();
+             if(rem!=null){
+            System.out.print(rem.data+" ");
+                  
+            if(rem.left!=null){
+                ans.add(rem.left);
+            }
+             if(rem.right!=null){
+                ans.add(rem.right);
+            }
+           
+             }else{
+                if( ans.size() >0 ){
+                     System.out.println();
+                    ans.add(rem);
+                }
+             }
+            
+        }
+        
+    }
+  }
